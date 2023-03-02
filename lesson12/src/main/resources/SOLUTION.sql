da65541ea4e9927b55675428a1089ebdec8bff9b
tabletka97 <mtabletka.by@gmail.com>
2023-03-02 11:58:45 +0300

    'fix first delete and month' try

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
-delete from STUDENT  WHERE GROUPNUMBER >= 4;
+delete from STUDENT  WHERE STUDENT.ID in (select distinct STUDENT_ID from MARK join SUBJECT S on S.ID = MARK.SUBJECT_ID where GRADE >= 4);
 delete from STUDENT where ID in (select distinct STUDENT_ID from MARK where MARK < 4);
 delete from PAYMENTTYPE where NAME = 'DAILY';
 delete from MARK where MARK < 7;
--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -2,6 +2,6 @@ select * from PAYMENT where AMOUNT >= 500;
 select * from STUDENT where BIRTHDAY < dateadd('YEAR', -20, '2023-03-02');
 select * from STUDENT where GROUPNUMBER = 10 and BIRTHDAY > dateadd('YEAR', -20, '2023-03-02');
 select * from STUDENT where NAME like 'Mike%' or GROUPNUMBER between 4 and 6;
-select * from PAYMENT where PAYMENT_DATE >= dateadd('MONTH', -8, '2023-03-02');
+SELECT * FROM payment WHERE DATEADD('MONTH', -8, CURRENT_DATE) < payment_date;
 select * from STUDENT where NAME like 'A%';
 select * from STUDENT where (NAME like 'Roxi%' and GROUPNUMBER = 4) or NAME like 'Tallie' and  GROUPNUMBER = 9;
